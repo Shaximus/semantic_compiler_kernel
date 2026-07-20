c4ee242a669f2e60b222e66b27022630d84d4901
Shaximus <shaximusoverride6@gmail.com>
2026-07-20 20:01:18 -0300

    feat: export buildcraft ontology registry

--- registry/__init__.py
+++ registry/__init__.py
@@ -1,1 +1,13 @@
 """Registry module - term registry, rules, departments, isomorphisms, negative samples."""
+
+from semantic_compiler.registry.buildcraft import (
+    BUILDCRAFT_MAPPINGS,
+    BuildcraftMapping,
+    get_buildcraft_mapping,
+)
+
+__all__ = [
+    "BUILDCRAFT_MAPPINGS",
+    "BuildcraftMapping",
+    "get_buildcraft_mapping",
+]
